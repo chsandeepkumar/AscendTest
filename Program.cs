b59c3a437896cf6a61929feda3b4a4f836db31be
chsandeepkumar <ch.sandeepreddy544@gmail.com>
2020-07-04 16:23:09 -0500

    updated

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using AscendTest.Library;
 
-namespace ConsoleApp3
+namespace AscendTest.ReadOnlyCollection
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            //var result = ValidationService.Validate("Abcd123_");
-            var result = ValidationService.Validate("_Abcd123_");
+            var list = new List<int> { 1, 2, 3 };
 
-            Console.WriteLine(result);
+            var sp = new SnapShot(list);
+
+            list.Add(4);
+
+            var restoreList = sp.Restore();
+            //restoreList is IReadOnlyCollection, so we can not add to the ReadOnlyCollection
+
+            Console.WriteLine(string.Join(",", restoreList));
             Console.ReadLine();
         }
     }
